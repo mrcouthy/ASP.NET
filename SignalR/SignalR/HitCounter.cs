b361276968908af58b620150e78cf120ca9f6caf
DJ <mrcouthy@gmail.com>
2015-08-11 17:56:57 +0545

    First Signal R project "Hit count works!"

---
+++ SignalR/SignalR/HitCounter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR
+{
+    public class HitCounter : Hub
+    {
+        static int hitCount = 0;
+        public void RecordHit()
+        {
+            hitCount += 1;
+            Clients.All.onRecordHit(hitCount);
+        }
+    }
+}
