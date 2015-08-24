f8ef6e52589a68fd7100586e9b62ffb49001a8b1
DJ <mrcouthy@gmail.com>
2015-08-24 18:33:43 +0545

    Added two projects no need of two , client can be a plain html ! just added some nuget in both not much code yet!
    Owin self host is there so no need of iis also
     app.UseCors(CorsOptions.AllowAll); is used so all domains can access this server ... cross domain !!!

---
+++ SignalRServer/ClockClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClockClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ClockClient")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8117f34b-8a38-48d3-b1d2-2cd91f354174")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SignalRServer/SignalRServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+ 
+
+namespace SignalR.Hosting.Self.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "http://127.0.0.1:8088/";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Started server on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
