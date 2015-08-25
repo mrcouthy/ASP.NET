351990b4a4efcc06f19933d51ba7567f1f3af427
DJ <mrcouthy@gmail.com>
2015-08-25 10:54:27 +0545

    I can now stop time !!!

--- SignalRServer/SignalRServer/ClockHub.cs
+++ SignalRServer/SignalRServer/ClockHub.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
+ using System.Timers;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 
@@ -10,9 +10,19 @@ namespace SignalRServer
 {
     public class ClockHub : Hub
     {
+        private Timer _timer;
         public override Task OnConnected()
         {
-            Clients.All.showTime("Hello time");
+            if (_timer ==null)
+            {
+                _timer = new Timer(1000);
+
+                _timer.Elapsed += (s, e) =>
+                {
+                    Clients.All.showTime(DateTime.Now.ToString());
+                };
+                _timer.Start();
+            }
             return base.OnConnected();
         }
        
