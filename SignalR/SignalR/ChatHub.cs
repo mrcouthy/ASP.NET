e3dc57818a0255a1c937d718acc6a6efeae7fa7f
DJ <mrcouthy@gmail.com>
2015-08-26 10:12:08 +0545

    Enabled Group Chat

--- SignalR/SignalR/ChatHub.cs
+++ SignalR/SignalR/ChatHub.cs
@@ -9,12 +9,12 @@ namespace SignalRChat
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        public void Send(string groupName,string name, string message)
         {
             // Call the broadcastMessage method to update clients.
             //      Clients.User("Dhiraj").broadcastMessage(name, message);
 
-            Clients.Group(name).broadcastMessage(name, message);
+            Clients.Group(groupName).broadcastMessage(name, message);
         }
 
         public void AddToGroup(string name)
