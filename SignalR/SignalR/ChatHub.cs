aea565ba7fc55de0e9ece7726a294c865689e505
DJ <mrcouthy@gmail.com>
2015-08-26 09:54:18 +0545

    Adding Chat

---
+++ SignalR/SignalR/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            //      Clients.User("Dhiraj").broadcastMessage(name, message);
+
+            Clients.Group(name).broadcastMessage(name, message);
+        }
+
+        public void AddToGroup(string name)
+        {
+            Groups.Add(Context.ConnectionId, name);
+        }
+    }
+}
